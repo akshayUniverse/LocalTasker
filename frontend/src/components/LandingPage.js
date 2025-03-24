5f262e0a195c809b6701155a4557e8cc0d84720c
Akshay Karada <akshaykarada72.o@gmail.com>
2025-03-25 01:55:50 +0530

    design landingpage

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './LandingPage.css';
 import { useNavigate } from 'react-router-dom'; 
 import { Button } from 'primereact/button';
+import heroImage from '../image/hero.jpg';
+import plumbingImg from '../image/plumbing.jpg';
+import ElectricalImg from '../image/electrical.jpg';
+import PaintingImg from '../image/painting.jpg';
 
 function LandingPage() {
   const navigate = useNavigate();
@@ -10,7 +14,7 @@ function LandingPage() {
       {/* Navigation Bar */}
       
       <nav className="navbar">
-        <div className="logo">LocalTasker</div>
+        <div className="logo">Local Tasker</div>
         <ul className="nav-links">
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
@@ -25,13 +29,14 @@ function LandingPage() {
       </nav>
 
       {/* Hero Section */}
-      <section id="home" className="hero">
+      <section id="home" className="hero-section" >
+        <div className="hero-content">
         <h1>Are you here to do work or get work done?</h1>
         <p>Join our platform to connect with skilled professionals or find work easily.</p>
+        </div>
         <div className="cta-buttons">
           <button className="work-btn">Do Work</button>
           <button className="hire-btn">Get Work Done</button>
-          
         </div>
       </section>
 
@@ -40,14 +45,17 @@ function LandingPage() {
         <h2>Our Services</h2>
         <div className="cards">
           <div className="card">
+          <img src={plumbingImg}  alt="Plumbing Service" className="card-image" />
             <h3>Plumbing</h3>
             <p>Expert plumbing services for homes and offices.</p>
           </div>
           <div className="card">
+            <img src={ElectricalImg} alt="Electrical Service" className="card-image" />
             <h3>Electrical</h3>
             <p>Certified electricians ready to assist.</p>
           </div>
           <div className="card">
+      <img src={PaintingImg} alt="Painting Service" className="card-image" />
             <h3>Painting</h3>
             <p>High-quality painting services for your space.</p>
           </div>
